f17f0e401ab3abc4849f0452dba8fb33b147c427
Dmitrii <dimana1507@gmail.com>
2018-04-16 18:57:13 +0300

    Add files via upload

--- app/js/app.js
+++ app/js/app.js
@@ -1,26 +1,77 @@
 let quizApp = angular.module('quizApp', []);
 
 quizApp.controller('quizDataCtrl',  function quizDataCtrl($scope){
+    
     $scope.datas = [
         {
             id: 0,
             question: "1. Which kitchen shape most closely represents your kitchen?",
+            selected : true,
             variants: [{
-                id: 0,
+                id: 1,
                 img: "../app/img/11.JPG",
                 text: "U-Shaped"
             },
             {
-                id: 1,
+                id: 2,
                 img: "../app/img/12.JPG",
-                text: "Straight"
+                text: "Straight with Island"
             },
             {
-                id: 2,
+                id: 3,
                 img: "../app/img/13.JPG",
                 text: "L-Shaped"
             }
+            
+            
+            ]
+        },
+        {
+            id: 1,
+            question: "2. Choose the design level of your kitchen?",
+            selected : false,   
+            variants: [{
+                id: 1,
+                img: "../app/img/21.png",
+                text: "Advanced"
+            },
+            {
+                id: 2,
+                img: "../app/img/22.png",
+                text: "Intermediate"
+            },
+            {
+                id: 3,
+                img: "../app/img/23.png",
+                text: "Basic"
+            }
+            
+            
+            ]
+        },
+        {
+            id: 1,
+            question: "3. Choose the design level of your kitchen?",
+            selected : false,   
+            variants: [{
+                id: 1,
+                img: "../app/img/21.png",
+                text: "Advanced"
+            },
+            {
+                id: 2,
+                img: "../app/img/22.png",
+                text: "Intermediate"
+            },
+            {
+                id: 3,
+                img: "../app/img/23.png",
+                text: "Basic"
+            }
+            
+            
             ]
         }
+        
     ]
 });
--- app/js/quiz__directive.js
+++ app/js/quiz__directive.js
@@ -2,16 +2,48 @@ quizApp.directive('quiz', function () {
     return {
         restrict: 'E',
         templateUrl: '../app/js/quiz.tpl.html',
-        controller: function($scope, $element){
-            $scope.checkControl = function($element){
-                console.log(this.$index);
+        controller: function ($scope, $element, $timeout) {
+            let counter = 0;
+            $scope.checkControl = function ($element) {
+
+                let answer = this.$index + 1;
+                $timeout(function () {
+                    $scope.datas[counter].selected = !$scope.datas[counter].selected;
+                    $scope.selected = !$scope.datas[counter].selected;
+
+                    counter++;
+                    if (counter >= $scope.datas.length) {
+                        console.log('In ' + counter + ' question user chose ' + answer);
+                        console.log('that`s all')
+                    }
+                    else {
+                        $scope.datas[counter].selected = !$scope.datas[counter].selected;
+                        $scope.selected = !$scope.datas[counter].selected;
+                        console.log('In ' + counter + ' question user chose ' + answer);
+                    }
+                }, 1000)
+
+
+
+
             }
+
+
         },
         link: function ($scope, $element) {
-            let elemCounter = $scope.datas[0].variants.length;
-            let selector = $element[0].querySelector('.variants');
-            console.log(selector)
-            
+            let elemWidth = 100 / $scope.datas[0].variants.length;
+
+            $scope.objWidth = {
+                "width": `${elemWidth}%`
+            };
+           
+                console.log($element[0].getElementsByClassName('pointsCircle'));
+                
+           
+
+
+
+            //ng-repeat includes points, that`s why we use setTimeout to catch element by className
         }
     }
 })

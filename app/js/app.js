6dc772a7d981363d728ae65b3bda14e869e1650e
Dmitrii <dimana1507@gmail.com>
2018-04-17 19:15:05 +0300

    Add files via upload

--- app/js/app.js
+++ app/js/app.js
@@ -1,77 +1,107 @@
 let quizApp = angular.module('quizApp', []);
 
-quizApp.controller('quizDataCtrl',  function quizDataCtrl($scope){
-    
+quizApp.controller('quizDataCtrl', function quizDataCtrl($scope) {
+
     $scope.datas = [
         {
-            id: 0,
+            id: 1,
             question: "1. Which kitchen shape most closely represents your kitchen?",
-            selected : true,
+            selected: true,
             variants: [{
-                id: 1,
+                question_id: 0,
                 img: "../app/img/11.JPG",
                 text: "U-Shaped"
             },
             {
-                id: 2,
+                question_id: 0,
                 img: "../app/img/12.JPG",
                 text: "Straight with Island"
             },
             {
-                id: 3,
+                question_id: 0,
                 img: "../app/img/13.JPG",
                 text: "L-Shaped"
             }
-            
-            
             ]
         },
         {
-            id: 1,
+            id: 2,
             question: "2. Choose the design level of your kitchen?",
-            selected : false,   
+            selected: false,
             variants: [{
-                id: 1,
+                question_id: 1,
                 img: "../app/img/21.png",
                 text: "Advanced"
             },
             {
-                id: 2,
+                question_id: 1,
                 img: "../app/img/22.png",
                 text: "Intermediate"
             },
             {
-                id: 3,
+                question_id: 1,
                 img: "../app/img/23.png",
                 text: "Basic"
             }
-            
-            
             ]
         },
         {
-            id: 1,
+            id: 3,
             question: "3. Choose the design level of your kitchen?",
-            selected : false,   
+            selected: false,
             variants: [{
-                id: 1,
-                img: "../app/img/21.png",
+                question_id: 2,
+                img: "../app/img/31.jpg",
                 text: "Advanced"
             },
             {
-                id: 2,
-                img: "../app/img/22.png",
+                question_id: 2,
+                img: "../app/img/32.jpg",
                 text: "Intermediate"
-            },
-            {
-                id: 3,
-                img: "../app/img/23.png",
-                text: "Basic"
             }
-            
-            
             ]
         }
-        
+        ,
+        {
+
+            id: 4,
+            question: "4. What type of countertop do you envision in your dream kitchen?",
+            selected: false,
+            variants: [{
+                question_id: 3,
+                img: "../app/img/41.jpg"
+                
+            },
+            {
+                question_id: 3,
+                img: "../app/img/42.jpg"
+                
+            },
+            {
+                question_id: 3,
+                img: "../app/img/43.jpg"
+                
+            }
+            ]
+        },
+        {
+            id: 5,
+            question: "5. What type of flooring do you envision in your dream kitchen?",
+            selected: false,
+            variants: [{
+                question_id: 4,
+                img: "../app/img/51.jpg",
+            },
+            {
+                question_id: 4,
+                img: "../app/img/52.jpg",
+            },
+            {
+                question_id: 4,
+                img: "../app/img/43.jpg",
+            }
+            ]
+        }
+
     ]
 });
--- app/js/quiz__directive.js
+++ app/js/quiz__directive.js
@@ -3,31 +3,54 @@ quizApp.directive('quiz', function () {
         restrict: 'E',
         templateUrl: '../app/js/quiz.tpl.html',
         controller: function ($scope, $element, $timeout) {
-            let counter = 0;
-            $scope.checkControl = function ($element) {
 
-                let answer = this.$index + 1;
-                $timeout(function () {
-                    $scope.datas[counter].selected = !$scope.datas[counter].selected;
-                    $scope.selected = !$scope.datas[counter].selected;
-
-                    counter++;
-                    if (counter >= $scope.datas.length) {
-                        console.log('In ' + counter + ' question user chose ' + answer);
-                        console.log('that`s all')
-                    }
-                    else {
-                        $scope.datas[counter].selected = !$scope.datas[counter].selected;
-                        $scope.selected = !$scope.datas[counter].selected;
-                        console.log('In ' + counter + ' question user chose ' + answer);
-                    }
-                }, 1000)
+            let answerData = [];
 
 
+            $scope.goTo = function () {
 
-
+                for (let i = 0; i < $scope.datas.length; i++) {
+                    $scope.datas[i].selected = false;
+                }
+                $scope.datas[this.$index].selected = !$scope.datas[this.$index].selected;
+                $scope.selected = !$scope.datas[this.$index].selected;
             }
 
+
+            $scope.checkControl = function (slide) {
+
+                let answer = this.$index;
+
+                let object = {};
+                for (let i = 0; i < $scope.datas[slide].variants.length; i++) {
+                    $scope.datas[slide].variants[i].selected = false;
+                }
+                $scope.datas[slide].variants[this.$index].selected = !$scope.datas[slide].variants[this.$index].selected
+
+
+                $timeout(function () {
+                    $scope.datas[slide].selected = !$scope.datas[slide].selected;
+                    $scope.selected = !$scope.datas[slide].selected;
+
+                    object.question = slide + 1;
+                    object.answer = answer;
+                    answerData[slide] = object;
+
+                    slide++;
+                    if (slide >= $scope.datas.length) {
+
+                        console.log('That`s all!')
+                        console.log(answerData);
+
+                    }
+                    else {
+                        $scope.datas[slide].selected = !$scope.datas[slide].selected;
+                        $scope.selected = !$scope.datas[slide].selected;
+
+                    }
+                }, 500)
+
+            }
 
         },
         link: function ($scope, $element) {
@@ -36,14 +59,8 @@ quizApp.directive('quiz', function () {
             $scope.objWidth = {
                 "width": `${elemWidth}%`
             };
-           
-                console.log($element[0].getElementsByClassName('pointsCircle'));
-                
-           
 
 
-
-            //ng-repeat includes points, that`s why we use setTimeout to catch element by className
         }
     }
 })
